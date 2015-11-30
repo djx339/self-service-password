8f6043208e7aed278c7161f83e62f697d1c70297
Daniel D <djx339@gmail.com>
2015-11-29 21:52:56 -0500

    Add some params changed when docker run

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,30 @@ LDAP_BINDPW="${LDAP_BINDPW:-}"
 LDAP_BASE="${LDAP_BASE:-}"
 LDAP_LOGIN_ATTRIBUTE="${LDAP_LOGIN_ATTRIBUTE:-uid}"
 LDAP_FULLNAME_ATTRIBUTE="${LDAP_FULLNAME_ATTRIBUTE:-cn}"
-LDAP_FILTER="${LDAP_FILTER:-(&(objectClass=person)(\$ldap_login_attribute={login}))}"
+LDAP_FILTER="${LDAP_FILTER:-\(\&\(objectClass=person\)\(\$ldap_login_attribute=\{login\}\)\)}"
+
+AD_MODE="${AD_MODE:-false}"
+AD_FORCE_UNLOCK="${AD_FORCE_UNLOCK:-false}"
+AD_FORCE_PWD_CHANGE="${AD_FORCE_PWD_CHANGE:-false}"
+SAMBA_MODE="${SAMBA_MODE:-false}"
+
+PWD_HASH="${PWD_HASH:-clear}"
+PWD_MIN_LENGTH="${PWD_MIN_LENGTH:-8}"
+PWD_MAX_LENGTH="${PWD_MAX_LENGTH:-32}"
+PWD_MIN_LOWER="${PWD_MIN_LOWER:-0}"
+PWD_MIN_UPPER="${PWD_MIN_UPPER:-0}"
+PWD_MIN_DIGIT="${PWD_MIN_DIGIT:-0}"
+PWD_MIN_SPECIAL="${PWD_MIN_SPECIAL:-0}"
+PWD_NO_REUSE="${PWD_NO_REUSE:-true}"
+PWD_COMPLEXITY="${PWD_COMPLEXITY:-0}"
+PWD_SHOW_POLICY="${PWD_SHOW_POLICY:-never}"
+
+SMTP_HOST="${SMTP_HOST:-}"
+SMTP_PORT="${SMTP_PORT:-25}"
+SMTP_AUTH="${SMTP_AUTH:-false}"
+SMTP_USERNAME="${SMTP_USERNAME:-x}"
+SMTP_PASSWORD="${SMTP_PASSWORD:-x}"
+
 
 if [[ -z "$LDAP_URL" ]]; then
     echo "LDAP_URL is required."
@@ -45,6 +68,25 @@ sed -e 's#{{LDAP_URL}}#'"${LDAP_URL}"'#g' \
     -e 's/{{LDAP_LOGIN_ATTRIBUTE}}/'"${LDAP_LOGIN_ATTRIBUTE}"'/g' \
     -e 's/{{LDAP_FULLNAME_ATTRIBUTE}}/'"${LDAP_FULLNAME_ATTRIBUTE}"'/g' \
     -e 's/{{LDAP_FILTER}}/'"${LDAP_FILTER}"'/g' \
+    -e 's/{{AD_MODE}}/'"${AD_MODE}"'/g' \
+    -e 's/{{AD_FORCE_UNLOCK}}/'"${AD_FORCE_UNLOCK}"'/g' \
+    -e 's/{{AD_FORCE_PWD_CHANGE}}/'"${AD_FORCE_PWD_CHANGE}"'/g' \
+    -e 's/{{SAMBA_MODE}}/'"${SAMBA_MODE}"'/g' \
+    -e 's/{{PWD_HASH}}/'"${PWD_HASH}"'/g' \
+    -e 's/{{PWD_MIN_LENGTH}}/'"${PWD_MIN_LENGTH}"'/g' \
+    -e 's/{{PWD_MAX_LENGTH}}/'"${PWD_MAX_LENGTH}"'/g' \
+    -e 's/{{PWD_MIN_LOWER}}/'"${PWD_MIN_LOWER}"'/g' \
+    -e 's/{{PWD_MIN_UPPER}}/'"${PWD_MIN_UPPER}"'/g' \
+    -e 's/{{PWD_MIN_DIGIT}}/'"${PWD_MIN_DIGIT}"'/g' \
+    -e 's/{{PWD_MIN_SPECIAL}}/'"${PWD_MIN_SPECIAL}"'/g' \
+    -e 's/{{PWD_NO_REUSE}}/'"${PWD_NO_REUSE}"'/g' \
+    -e 's/{{PWD_COMPLEXITY}}/'"${PWD_COMPLEXITY}"'/g' \
+    -e 's/{{PWD_SHOW_POLICY}}/'"${PWD_SHOW_POLICY}"'/g' \
+    -e 's/{{SMTP_HOST}}/'"${SMTP_HOST}"'/g' \
+    -e 's/{{SMTP_PORT}}/'"${SMTP_PORT}"'/g' \
+    -e 's/{{SMTP_AUTH}}/'"${SMTP_AUTH}"'/g' \
+    -e 's/{{SMTP_USERNAME}}/'"${SMTP_USERNAME}"'/g' \
+    -e 's/{{SMTP_PASSWORD}}/'"${SMTP_PASSWORD}"'/g' \
     -i $CONF_FILE
 
 exec "$@"
